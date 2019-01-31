4e8c4d14a99b460f52cc75084cc8a6446b28e0e7
Pornthip Sae-Chong <pornthip.sae-chong@razer.com>
2019-01-31 18:02:35 +0800

    define game and players

--- src/main.go
+++ src/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"math/rand"
+	"time"
 )
 
 type Server struct{}
@@ -11,9 +13,9 @@ type Server struct{}
 func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r)
 	if r.Method == "GET" {
-		board := Board{}
-		board = board.Start()
-		fmt.Println(board)
+		game := Game{}
+		game = game.Start(2)
+		fmt.Println(game)
 		fmt.Fprint(w, "<h1>Test</h1>") // Fprint write to a buffer output
 	}
 }
@@ -28,6 +30,13 @@ func checkError (err error) {
 	if err != nil {
 		panic(err) // panic will stop excution
 	}
+}
+
+type Game struct {
+	Board Board
+	Players []Player
+	Tikis []Tiki
+	Discard []Card
 }
 
 type Tiki struct{
@@ -65,7 +74,7 @@ type Player struct {
 	Secret SecretCard
 }
 
-func (b Board) Start() Board {
+func (g Game) Start(playerNumber int) Game {
 	t1 := Tiki{"Red","1","r1"}
 	t2 := Tiki{"Orange","1","o1"}
 	t3 := Tiki{"Yellow","1","y1"}
@@ -75,6 +84,75 @@ func (b Board) Start() Board {
 	t7 := Tiki{"Grey","3","g3"}
 	t8 := Tiki{"Pink","3","p3"}
 	t9 := Tiki{"White","3","w3"}
-	startBoard := Board{t1,t2,t3,t4,t5,t6,t7,t8,t9}
-	return startBoard
+	g1 := []Tiki{t1,t2,t3}
+	g2 := []Tiki{t4,t5,t6}
+	g3 := []Tiki{t7,t8,t9}
+
+	b := Board{}
+	g.Board = b.New(g1,g2,g3)
+
+	p := Player{}
+	p = p.New()
+	for i := 0; i<playerNumber; i++ {
+		g.Players = append(g.Players, p)
+	}
+	fmt.Println(&p, &g.Players[0], &g.Players[1])
+	
+	return g
 }
+
+func (b Board) New(g1 , g2, g3 []Tiki) Board {
+	g1 = Shuffle(g1)
+	g2 = Shuffle(g2)
+	g3 = Shuffle(g3)
+	fmt.Println(g1,g2,g3)
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	perm := r.Perm(3)
+	fmt.Println(perm)
+	gA := [][]Tiki {g1,g2,g3}
+	b.Position1 = gA[perm[0]][0]
+	b.Position2 = gA[perm[0]][1]
+	b.Position3 = gA[perm[0]][2]
+	b.Position4 = gA[perm[1]][0]
+	b.Position5 = gA[perm[1]][1]
+	b.Position6 = gA[perm[1]][2]
+	b.Position7 = gA[perm[2]][0]
+	b.Position8 = gA[perm[2]][1]
+	b.Position9 = gA[perm[2]][2]
+	return b	
+}
+
+func (p Player) New() Player {
+	c1 := Card{"Move1","UP",1}
+	c2 := Card{"Move2","UP",2}
+	c3 := Card{"Move3","UP",3}
+	cx := Card{"X","X",1}
+	cdrop := Card{"Drop","DROP",0}
+	p.Cards = make([]Card, 7, 7)
+	p.Cards = append(p.Cards, c1)
+	p.Cards = append(p.Cards, c1)
+	p.Cards = append(p.Cards, c2)
+	p.Cards = append(p.Cards, c3)
+	p.Cards = append(p.Cards, cx)
+	p.Cards = append(p.Cards, cx)
+	p.Cards = append(p.Cards, cdrop)
+	return p
+}
+
+func Shuffle(t []Tiki) []Tiki {
+	fmt.Println(t)
+	fmt.Println(time.Now().Unix())
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for n:=len(t); n>0; n-- {
+		rInt:= r.Intn(len(t))
+		fmt.Println("random int ", rInt)
+		t[n-1], t[rInt] = t[rInt], t[n-1]
+	}
+	return t
+} 
+
+
+func (p Player) Move(c Card, t Tiki, b Board) {
+	// what type of card is used
+	
+} 
